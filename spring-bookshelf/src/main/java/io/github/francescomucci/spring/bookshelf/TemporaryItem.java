4769c0efd7cf7276b975c2ea5da435650089f3fd
Francesco Mucci <mucciacios@gmail.com>
2021-11-30 13:25:11 +0100

    Configured maven-surefire-plugin (to execute unit test during builds)

---
+++ spring-bookshelf/src/main/java/io/github/francescomucci/spring/bookshelf/TemporaryItem.java
@@ -0,0 +1,15 @@
+package io.github.francescomucci.spring.bookshelf;
+
+public class TemporaryItem {
+
+	private String hello;
+
+	public TemporaryItem(String hello) {
+		this.hello = hello;
+	}
+
+	public String sayHello() {
+		return hello;
+	}
+
+}
