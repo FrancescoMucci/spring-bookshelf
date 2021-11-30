8e31d6587893a62f43d2e5f7785d7657204e1595
Francesco Mucci <mucciacios@gmail.com>
2021-11-30 12:09:17 +0100

    Created project with spring-initializr and added comments in pom

---
+++ spring-bookshelf/src/main/java/io/github/francescomucci/spring/bookshelf/SpringBookshelfApplication.java
@@ -0,0 +1,13 @@
+package io.github.francescomucci.spring.bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBookshelfApplication.class, args);
+	}
+
+}
---
+++ spring-bookshelf/src/test/java/io/github/francescomucci/spring/bookshelf/SpringBookshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.francescomucci.spring.bookshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
