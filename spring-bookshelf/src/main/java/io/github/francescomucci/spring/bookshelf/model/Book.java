0c232d1f8df91c468f5a1f0a2cab10393b690540
Francesco Mucci <mucciacios@gmail.com>
2021-12-01 11:55:35 +0100

    Created domain model class

---
+++ spring-bookshelf/src/main/java/io/github/francescomucci/spring/bookshelf/model/Book.java
@@ -0,0 +1,66 @@
+package io.github.francescomucci.spring.bookshelf.model;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Book {
+
+	private Long isbn;
+
+	private String title;
+	private List<String> authors;
+
+	public Book(Long isbn, String title, List<String> authors) {
+		this.isbn = isbn;
+		this.title = title;
+		this.authors = authors;
+	}
+
+	public Long getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(Long isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public List<String> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<String> authors) {
+		this.authors = authors;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [isbn=" + isbn + ", title=" + title + ", authors=" + authors + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(authors, isbn, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(authors, other.authors) && Objects.equals(isbn, other.isbn)
+				&& Objects.equals(title, other.title);
+	}
+
+}
