d8d76c52f4393125ed8c7b1b57cea84d2f1e7aaf
Francesco Mucci <mucciacios@gmail.com>
2021-12-21 11:48:44 +0100

    Added BookData: DTO for book

---
+++ spring-bookshelf/src/main/java/io/github/francescomucci/spring/bookshelf/model/dto/BookData.java
@@ -0,0 +1,71 @@
+package io.github.francescomucci.spring.bookshelf.model.dto;
+
+import static java.util.Arrays.asList;
+
+import java.util.Objects;
+
+import io.github.francescomucci.spring.bookshelf.model.Book;
+
+public class BookData extends IsbnData {
+
+	private String title;
+
+	private String authors;
+
+	public BookData() {
+		super();
+	}
+
+	public BookData(String isbn, String title, String authors) {
+		super(isbn);
+		this.title = title;
+		this.authors = authors;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(String authors) {
+		this.authors = authors;
+	}
+
+	public Book toBook() {
+		return new Book(
+			toLong(),
+			title,
+			asList(authors.split(", ")));
+	}
+
+	@Override
+	public String toString() {
+		return "BookData [isbn=" + getIsbn() + ", title=" + title + ", authors=" + authors + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(authors, getIsbn(), title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BookData other = (BookData) obj;
+		return Objects.equals(authors, other.authors) && Objects.equals(getIsbn(), other.getIsbn())
+				&& Objects.equals(title, other.title);
+	}
+
+}
