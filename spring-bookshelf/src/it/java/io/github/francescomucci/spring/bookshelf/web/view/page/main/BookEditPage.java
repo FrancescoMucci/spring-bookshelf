0f2acb30eb7b3065f8fba0cfedc24ad755a1afbe
Francesco Mucci <mucciacios@gmail.com>
2022-01-13 11:53:13 +0100

    BookEditView integration tests

--- spring-bookshelf/src/it/java/io/github/francescomucci/spring/bookshelf/web/view/page/main/BookEditPage.java
+++ spring-bookshelf/src/it/java/io/github/francescomucci/spring/bookshelf/web/view/page/main/BookEditPage.java
@@ -2,14 +2,21 @@ package io.github.francescomucci.spring.bookshelf.web.view.page.main;
 
 import org.openqa.selenium.WebDriver;
 
+import io.github.francescomucci.spring.bookshelf.web.view.page.IPageWithForm;
 import io.github.francescomucci.spring.bookshelf.web.view.page.MyPage;
 
-public class BookEditPage extends MyPage {
+public class BookEditPage extends MyPage implements IPageWithForm {
 
 	private static final String EXPECTED_TITLE = "Book edit view";
 
 	public BookEditPage(WebDriver webDriver) {
 		super(webDriver, EXPECTED_TITLE);
+	}
+
+	public MyPage fillEditFormAndPressSubmitButton(String title, String authors) {
+		clearAndThenfillFormInput(this,"title", title);
+		clearAndThenfillFormInput(this,"authors", authors);
+		return pressSubmitButton(this);
 	}
 
 }
