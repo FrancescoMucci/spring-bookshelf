485a9e3198be5cddffa2aa90e06b99fba84586ab
Francesco Mucci <mucciacios@gmail.com>
2022-01-17 16:06:04 +0100

    SpringBookshelfApplicationE2E showBookList tests

--- spring-bookshelf/src/e2e/java/io/github/francescomucci/spring/bookshelf/SpringBookshelfApplicationE2E.java
+++ spring-bookshelf/src/e2e/java/io/github/francescomucci/spring/bookshelf/SpringBookshelfApplicationE2E.java
@@ -1,9 +1,10 @@
 package io.github.francescomucci.spring.bookshelf;
 
-
+import static io.github.francescomucci.spring.bookshelf.BookTestingConstants.*;
 import static io.github.francescomucci.spring.bookshelf.web.security.WebSecurityTestingConstants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.bson.Document;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -12,20 +13,35 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import io.github.francescomucci.spring.bookshelf.web.view.page.main.BookHomePage;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.francescomucci.spring.bookshelf.model.Book;
+import io.github.francescomucci.spring.bookshelf.web.view.page.main.BookHomePage;
+import io.github.francescomucci.spring.bookshelf.web.view.page.main.BookListPage;
 
 /* Before run this test, make sure your SpringBoot application and MongoDB are up and running */
 
 @RunWith(SpringRunner.class)
+@TestPropertySource("classpath:application.properties")
 public class SpringBookshelfApplicationE2E {
 
 	private static final int PORT_NUMBER = Integer.parseInt(System.getProperty("server.port", "8080"));
 	private static final String BASE_URL = "http://localhost:" + PORT_NUMBER;
+	private static final String DB_COLLECTION = "Book";
 
+	@Value("${spring.data.mongodb.port:27017}")
+	private int db_port_number;
+
+	@Value("${spring.data.mongodb.database:test}")
+	private String db_name;
+
+	private MongoClient mongoClient;
 	private WebDriver webDriver;
 	private ChromeOptions options;
 	private BookHomePage bookHomePage;
@@ -37,6 +53,9 @@ public class SpringBookshelfApplicationE2E {
 
 	@Before
 	public void setup() {
+		mongoClient = MongoClients.create("mongodb://localhost:" + db_port_number);
+		mongoClient.getDatabase(db_name).drop();
+		
 		options = new ChromeOptions();
 		options.addArguments("start-maximized", "user-data-dir=target/web-driver-data/chrome-user-data");
 		webDriver = new ChromeDriver(options);
@@ -48,6 +67,9 @@ public class SpringBookshelfApplicationE2E {
 	public void teardown() {
 		webDriver.manage().deleteCookieNamed(REMEMBER_ME_TOKEN);
 		webDriver.quit();
+		
+		mongoClient.getDatabase(db_name).drop();
+		mongoClient.close();
 	}
 
 	/* ---------- SpringBookshelfApplication login tests ---------- */
@@ -126,6 +148,40 @@ public class SpringBookshelfApplicationE2E {
 			.contains("Welcome to my book library");
 		assertThat(bookHomePage.getLogoutMessage())
 			.isEqualTo("Logged out successfully");
+	}
+
+	/* ---------- SpringBookshelfApplication showBookList tests ---------- */
+
+	@Test
+	public void testSpringBookshelfApplication_showBookList_whenNoBookAdded_shouldShowEmptyMessage() {
+		BookListPage bookListPage = (BookListPage) bookHomePage.clickNavbarShowBookListLink();
+		
+		assertThat(bookListPage.getHeaderContent())
+			.contains("Empty database");
+	}
+
+	@Test
+	public void testSpringBookshelfApplication_showBookList_whenSomeBookAdded_shouldShowBooksInATables() {
+		setupAddingBookToDatabase(
+			new Book(VALID_ISBN13, TITLE, AUTHORS_LIST));
+		setupAddingBookToDatabase(
+			new Book(VALID_ISBN13_2, TITLE_2, AUTHORS_LIST_2));
+		
+		BookListPage bookListPage = (BookListPage) bookHomePage.clickNavbarShowBookListLink();
+		
+		assertThat(bookListPage.getBookTable())
+			.contains(VALID_ISBN13_WITHOUT_FORMATTING, TITLE, AUTHORS_STRING)
+			.contains(VALID_ISBN13_2_WITHOUT_FORMATTING, TITLE_2, AUTHORS_STRING_2);
+	}
+
+	/* ---------- Helper methods ---------- */
+
+	private void setupAddingBookToDatabase(Book book) {
+		mongoClient.getDatabase(db_name).getCollection(DB_COLLECTION)
+			.insertOne(new Document()
+				.append("_id", book.getIsbn())
+				.append("title", book.getTitle())
+				.append("authors", book.getAuthors()));
 	}
 
 }
